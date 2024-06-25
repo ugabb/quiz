c7a05d14486353bb631863ada76dd63f6f895e1b
Gabriel Barros <ugabrieldev@gmai.com>
2024-06-25 20:41:59 -0300

    fix(Header): show user profile only if authenticated

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -16,21 +16,23 @@ const Header = () => {
                         <Image src="/cnvv-logo.png" alt="Logo" width={200} height={200} />
                         <Image src="/penatrans.png" alt="Logo" width={200} height={200} />
                     </div>
-                    <details className="dropdown dropdown-end">
-                        <summary className="btn m-1">
-                            <div className="avatar placeholder gap-2 items-center">
-                                <div className="bg-blue-500 text-neutral-content w-8 rounded-full">
-                                    <span className="text-xs">{currentUser?.name[0].toUpperCase()}</span>
+                    {currentUser && (
+                        <details className="dropdown dropdown-end">
+                            <summary className="btn m-1">
+                                <div className="avatar placeholder gap-2 items-center">
+                                    <div className="bg-blue-500 text-neutral-content w-8 rounded-full">
+                                        <span className="text-xs">{currentUser?.name[0].toUpperCase()}</span>
+                                    </div>
+                                    <span className='text-sm text-zinc-800 font-semibold'>{currentUser?.name}</span>
                                 </div>
-                                <span className='text-sm text-zinc-800 font-semibold'>{currentUser?.name}</span>
-                            </div>
-                        </summary>
-                        <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-                            <li>
-                                <span className='text-sm font-semibold' onClick={logout}>Logout</span>
-                            </li>
-                        </ul>
-                    </details>
+                            </summary>
+                            <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                                <li>
+                                    <span className='text-sm font-semibold' onClick={logout}>Logout</span>
+                                </li>
+                            </ul>
+                        </details>
+                    )}
                 </header>
             ) : (
                 ''
